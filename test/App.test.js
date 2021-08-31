77bd785723d9a200e44dc77c4b0e135f85790198
Neha Aggarwal <neha.aggarwal@bbc.co.uk>
2021-08-31 11:02:41 +0100

    fix test

--- test/App.test.js
+++ test/App.test.js
@@ -21,36 +21,29 @@ describe('deleteData', () => {
     const view = shallow(<App deleteHandler={ mockProps.deleteHandler} isChecked={mockProps.isChecked}/>);
     view.find('button').at(3).simulate('click');
     axios.delete.mockImplementation(() => Promise.resolve({}))
-    expect(axios.delete).not.toHaveBeenCalledTimes(1);
+    expect(axios.delete).toHaveBeenCalledTimes(1);
 
   })
   it('should call axios read function on clicking read button', () => {
     const view = shallow(<App readHandler={mockProps.readHandler}isChecked={mockProps.isChecked}/>);
     view.find('button').at(2).simulate('click');
     axios.get.mockImplementation(() => Promise.resolve({}))
-    expect(axios.get).not.toHaveBeenCalledTimes(1);
+    expect(axios.get).toHaveBeenCalledTimes(1);
 
   })
   it('should call axios create function on clicking create button', () => {
     const view = shallow(<App createHandler={mockProps.createHandler}isChecked={mockProps.isChecked}/>);
     view.find('button').at(0).simulate('click');
     axios.create.mockImplementation(() => Promise.resolve({}))
-    expect(axios.create).not.toHaveBeenCalledTimes(1);
+    expect(axios.create).toHaveBeenCalledTimes(1);
 
   })
   it('should call axios replace function on clicking replace button', () => {
     const view = shallow(<App replaceHandler={mockProps.replaceHandler}isChecked={mockProps.isChecked}/>);
     view.find('button').at(1).simulate('click');
     axios.put.mockImplementation(() => Promise.resolve({}))
-    expect(axios.put).not.toHaveBeenCalledTimes(1);
+    expect(axios.put).toHaveBeenCalledTimes(1);
 
   })
  
-  it('fetches erroneously data from an API', async () => {
-    const errorMessage = 'Network Error';
- 
-    axios.get.mockImplementationOnce(() =>
-      Promise.reject(new Error(errorMessage)),
-    );
-  });
 });
